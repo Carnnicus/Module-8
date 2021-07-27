fb8490a741ef7f422133cad469542b50c3d38106
Carnnicus <78874425+Carnnicus@users.noreply.github.com>
2021-07-27 16:43:34 -0300

    Update App.py

--- App.py
+++ App.py
@@ -24,5 +24,16 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
-db.init_app(app)
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
+
+
 app.run(port=5000, debug = True)
